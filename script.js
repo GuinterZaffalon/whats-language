71df422976fa307b035ca5e35588fca92fba8e1d
Guinter Zaffalon <guinterzafa@gmail.com>
2023-10-05 15:58:40 -0300

    primeiro código js

---
+++ script.js
@@ -0,0 +1,10 @@
+var linguagens = [Javascript, Python, Java, PHP, C, Objectve-C, Go, Swift, Rubi, Kotlin, Perl, Rust, Perl]
+
+function sortearLang () {
+
+    var lingSorteada = Math.floor(Math.random()* linguagem.lenght);
+
+    var linguagemSorteada = linguagens[lingSorteada];
+
+    document.getElementById("linguagemSorteada").textContent = linguagemSorteada;
+}
