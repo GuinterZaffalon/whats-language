54aab46ba085c726747994f086de7d683dc0e6c3
Guinter Zaffalon <guinterzafa@gmail.com>
2023-10-05 22:07:24 -0300

    lista de langs aprimorada

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var linguagens = ["Javascript", "Python", "Java", "PHP", "C", "Objective-C", "Go", "Swift", "Ruby", "Kotlin", "Perl", "Rust", "Perl", "HTML"];
+var linguagens = [
+    "Python", "JavaScript", "HTML/CSS", "SQL", "Bash/Shell", "Java",
+    "C++", "C", "C#", "TypeScript", "PHP", "Rust", "PowerShell",
+    "Go", "Lua", "Assembly", "R", "Kotlin", "VBA", "MATLAB",
+    "Dart", "Visual Basic (.Net)", "Swift", "Ruby", "Delphi",
+    "Perl", "GDScript", "Haskell", "Julia", "Groovy", "Lisp",
+    "Fortran", "Objective-C", "Scala", "Elixir", "Zig", "Prolog",
+    "Solidity", "Cobol", "Ada", "Clojure", "OCaml", "Apex",
+    "Erlang", "SAS", "F#", "Nim", "APL", "Crystal", "Raku", "Flow"
+];
 
 function sortearLinguagens() {
     var indiceSorteado = Math.floor(Math.random() * linguagens.length);
